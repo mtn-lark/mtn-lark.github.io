ccef22abe0420a8d55058814b81974c42e28da61
Lark Mendoza <119717119+mtn-lark@users.noreply.github.com>
2024-06-03 02:27:39 -0700

    Remove sample JSON

--- art/script.js
+++ art/script.js
@@ -25,38 +25,6 @@
     let numFails = 0
     let shuffleButton = null
 
-    // SAMPLE ARTIC ART JSON RESPONSE
-    const DEMO_ART_DATA = {
-        "data": {
-            "id": 273752,
-            "title": "Jewelry",
-            "artist_display": "Ida Ou Nadif\nAnti-Atlas Mountains, Morocco\nNorthern Africa and the Sahel",
-            "place_of_origin": "Morocco",
-            "medium_display": "Silver, enamel, niello, glass, leather, and fabric",
-            "color": {
-                "h": 357,
-                "l": 41,
-                "s": 38,
-                "percentage": 0.001690602784422786,
-                "population": 12
-            },
-            "style_title": "ida ou Nadif",
-            "image_id": "0e4c9360-83b5-7624-4eb2-4f450f0864f2"
-        },
-        "info": {
-            "license_text": "The `description` field in this response is licensed under a Creative Commons Attribution 4.0 Generic License (CC-By) and the Terms and Conditions of artic.edu. All other data in this response is licensed under a Creative Commons Zero (CC0) 1.0 designation and the Terms and Conditions of artic.edu.",
-            "license_links": [
-                "https://creativecommons.org/publicdomain/zero/1.0/",
-                "https://www.artic.edu/terms"
-            ],
-            "version": "1.10"
-        },
-        "config": {
-            "iiif_url": "https://www.artic.edu/iiif/2",
-            "website_url": "http://www.artic.edu"
-        }
-    }
-
     window.addEventListener("load", init);
 
     //----------------------------------//
